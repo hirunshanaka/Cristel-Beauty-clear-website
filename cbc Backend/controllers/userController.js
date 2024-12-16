3e685cf1c233e3dba736673bec12064cb1e59df4
Hirun Rajapaksha <145091664+hirunshanaka@users.noreply.github.com>
2024-12-16 16:05:45 +0530

    login

--- cbc Backend/controllers/userController.js
+++ cbc Backend/controllers/userController.js
@@ -1,42 +1,76 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 
-export function craeteUser(req,res){
-    const newUserData=req.body
-    newUserData.password=bcrypt.hashSync(newUserData.password,10)
-   
-    //create database 
-   const user= new User(newUserData);
-   console.log(newUserData)
-   user.save().then(()=>{
+export function createUser(req, res) {
+  try {
+    const newUserData = req.body;
+    if (!newUserData.email || !newUserData.password) {
+      return res.json({
+        message: "Email and password are required",
+      });
+    }
+
+    if (newUserData.password.length < 8) {
+      return res.json({
+        message: "Password must be at least 8 characters long",
+      });
+    }
+
+    newUserData.password = bcrypt.hashSync(newUserData.password, 10);
+
+    const user = new User(newUserData);
+    console.log(newUserData);
+    user.save().then(() => {
+      res.json({
+        message: "User  created",
+      });
+    }).catch((error) => {
+      console.error(error);
+      res.json({
+        message: "User  not created",
+      });
+    });
+  } catch (error) {
+    console.error(error);
     res.json({
-        message:"user created"
-    })
-   }).catch(()=>{
-    res.json({
-        message:"user not created"
-      })
-  })
+      message: "An error occurred",
+    });
+  }
 }
 
-export function loginUser(req,res){
-    User.find({email:req.body.email}).then((users)=>{
-      if(users.length==0)   {
-        res.json({    
-            message:"user not found"
-        }) 
-      }else{
-        if(bcrypt.compareSync(req.body.password,users[0].password)){
-            res.json({
-                message:"user logged in"
-                })
-        }else{
-            res.json({
-                message:"user not logged in"
-            })
-        }
+export function loginUser(req, res) {
+  try {
+    if (!req.body.email || !req.body.password) {
+      return res.json({
+        message: "Email and password are required",
+      });
+    }
+
+    User.find({ email: req.body.email }).then((users) => {
+      if (users.length === 0) {
+        return res.json({
+          message: "User  not found",
+        });
       }
-        }
-      )
+    
+      const user = users[0];
+      const isPasswordMatch = bcrypt.compareSync(req.body.password, user.password);
+      if (isPasswordMatch) {
+        res.json({
+          message: "User  logged in",
+        });
+      } else {
+        res.json({
+          message: "User  not logged in",
+        });
+      }
+    }) 
+} 
+catch (error) {
+  console.error(error);
+  res.json({
+    message: "An error occurred",
+  });
 }
+} 
 
--- cbc Backend/routes/userRouter.js
+++ cbc Backend/routes/userRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { craeteUser} from "../controllers/userController.js";
-
+import { createUser, loginUser  } from "../controllers/userController.js";
 //create user Router
 const userRouter = express.Router();
 
-userRouter.post("/",craeteUser)
+userRouter.post("/",createUser)
+userRouter.post("/login",loginUser)
 
 export default userRouter
