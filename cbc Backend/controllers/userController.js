c9adfe5f1a1631f514179e3eae1db0c8ce3b54ff
Hirun Rajapaksha <145091664+hirunshanaka@users.noreply.github.com>
2025-01-20 12:14:15 +0530

    hiding secret key

--- cbc Backend/controllers/userController.js
+++ cbc Backend/controllers/userController.js
@@ -1,6 +1,8 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken"; 
+import dotenv from "dotenv";
+dotenv.config();
 export function createUser(req, res) {
   try {
     const newUserData = req.body;
@@ -64,7 +66,7 @@ export function loginUser(req, res) {
         type:user.type,
         profilepicture:user.profilepicture
         
-       },"cbc-secret-key-7973")
+       },process.env.SECRET);
        console.log(token)
        res.json({
          message: "User logged in",
