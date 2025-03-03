05b93e260673b025b4e97f5cbd9a44ce5286f87d
Hirun Rajapaksha <145091664+hirunshanaka@users.noreply.github.com>
2025-03-03 17:47:30 +0530

    modified

--- cbc Backend/controllers/orderController.js
+++ cbc Backend/controllers/orderController.js
@@ -1,4 +1,4 @@
-import Order from "../models/Order.js";
+import Order from "../models/order.js";
 import Product from "../models/product.js";
 import { isAdmin, isCustomer } from "./userController.js";
 
