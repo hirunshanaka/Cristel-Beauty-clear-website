b4f97295217f0c65b728ccbb161980ac44ee78f9
Hirun Rajapaksha <145091664+hirunshanaka@users.noreply.github.com>
2025-04-19 01:16:08 +0530

    upload images supabase (this is testing part text.jsx)

--- cbc-frontend/src/App.jsx
+++ cbc-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from './pages/home/loginPage'
 import SigninPage from './pages/home/signinPage'
 import AdminHomePage from './pages/home/adminHomePage'
 import { Toaster } from 'react-hot-toast'
+import FileUploadTest from './pages/home/test'
 //import UserData from './components/userData'
 function App() {
   return (
@@ -17,7 +18,8 @@ function App() {
           <Route path="/signin" element={<SigninPage />} />
           <Route path="/*" element={<HomePage />} />
           <Route path="/admin/*"element={<AdminHomePage/>}/>
-        </Routes>
+          <Route path="/Testing" element={<FileUploadTest/>} />
+         </Routes>
       </BrowserRouter>
     </>
   )
